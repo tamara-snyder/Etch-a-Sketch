dbc5ab208317740f92a5319839a8ad8b3f38cc91
badbassoonist <tamara.snyder@yale.edu>
2020-08-12 08:10:40 -0400

    Add working Etch-a-Sketch (default rainbow mode only)

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-let squares = 256;
 let container = document.getElementById('container');
+let gridSize = document.getElementById('grid-size');
+let clear = document.getElementById('clear');
+let shape = document.getElementById('shape');
+let square = document.getElementsByClassName('square');
+//let color = document.getElementById('color');
+let color;
 
+gridSize.addEventListener('change', reset);
+//color.addEventListener('change', makeGrid(gridSize));
+clear.addEventListener('click', reset);
 
-for (let i = 0; i < squares; i++) {
-  let div = document.createElement('div');
-  div.classList.add('square');
-  container.appendChild(div);
+function makeGrid(num) {
+  container.style.setProperty('--grid-rows', num);
+  container.style.setProperty('--grid-columns', num);
+  for (let i = 0; i < (num * num); i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseover', (e) => {
+      e.target.style.backgroundColor = getRandomColor();
+      });
+    container.appendChild(square);
+    }
 }
+
+
+function getRandomColor() {
+  const random = () => Math.floor(Math.random() * 256);
+  return `rgb(${random()}, ${random()}, ${random()})`;
+}
+
+function reset() {
+  let all = document.querySelectorAll('.square');
+  all.forEach(item => item.remove());
+  makeGrid(gridSize.value);
+}
+
+
+makeGrid(gridSize.value);
