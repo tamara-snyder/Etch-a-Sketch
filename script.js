97ec34f464ef0105d3025fe463d917554dd31c59
badbassoonist <tamara.snyder@yale.edu>
2020-08-12 10:50:16 -0400

    Remove extra features for a cleaner appearance

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 let container = document.getElementById('container');
 let gridSize = document.getElementById('grid-size');
 let clear = document.getElementById('clear');
-let shape = document.getElementById('shape');
-let square = document.getElementsByClassName('square');
-//let color = document.getElementById('color');
-let color;
 
 gridSize.addEventListener('change', reset);
-//color.addEventListener('change', makeGrid(gridSize));
 clear.addEventListener('click', reset);
 
 function makeGrid(num) {
@@ -16,13 +11,18 @@ function makeGrid(num) {
   for (let i = 0; i < (num * num); i++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    square.addEventListener('mouseover', (e) => {
-      e.target.style.backgroundColor = getRandomColor();
-      });
     container.appendChild(square);
     }
 }
 
+function colorSquares() {
+  const squares = container.querySelectorAll('.square');
+  
+  squares.forEach(square => square.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = getRandomColor();
+    })
+  );
+}
 
 function getRandomColor() {
   const random = () => Math.floor(Math.random() * 256);
@@ -33,7 +33,7 @@ function reset() {
   let all = document.querySelectorAll('.square');
   all.forEach(item => item.remove());
   makeGrid(gridSize.value);
+  colorSquares();
 }
 
-
-makeGrid(gridSize.value);
+reset();
